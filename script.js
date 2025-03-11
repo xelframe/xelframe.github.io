299aee677844f7090662ea51ac4e3100e10a73dd
Sigma Semka <bhzrd0@gmail.com>
2025-03-11 14:37:40 +0300

    писюн f6

--- script.js
+++ script.js
@@ -95,7 +95,6 @@ function digitalClock() {
 document.addEventListener('DOMContentLoaded', (event) => {
     digitalClock();
     
-    // Загрузка счетчика посещений
     try {
         const script = document.createElement('script');
         script.src = "//counter.websiteout.com/js/3/8/0/1";
@@ -132,3 +131,42 @@ if (!sessionStorage.getItem('animated')){
 } else {
     terminal.remove();
 }
+
+var arr = [
+       'https://my-wallpapers.github.io/my-wallpapers/Corner_of_my_desk.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Dark_forest.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Dark_park.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Ducks_in_the_sea.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Flower.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Flowers.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Grass.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Hill.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Insects.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Keys.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Leaf.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Leaves_in_the_rock.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Mountain.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Plank.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Sky.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Stairs.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Stones.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Stop.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Sun.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Sunset.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/Trees.jpg',
+       'https://my-wallpapers.github.io/my-wallpapers/lamp_dark.png'
+     ];
+     
+     function rand(min, max) {
+       return Math.floor(Math.random() * (max - min) + min);
+     }
+     
+     var savedWallpaper = sessionStorage.getItem('backgroundWallpaper');
+     
+     if (savedWallpaper) {
+       document.body.style.backgroundImage = 'url(' + savedWallpaper + ')';
+     } else {
+       var selectedWallpaper = arr[rand(0, arr.length)];
+       document.body.style.backgroundImage = 'url(' + selectedWallpaper + ')';
+       sessionStorage.setItem('backgroundWallpaper', selectedWallpaper);
+     }
